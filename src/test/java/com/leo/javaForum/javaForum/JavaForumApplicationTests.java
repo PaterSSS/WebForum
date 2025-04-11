29b78d7d35591fbfd3da0e670cd8b73cd5b37189
Leonid <leo.chel@mail.ru>
2025-04-11 13:14:41 +0300

    test asdf

--- src/test/java/com/leo/javaForum/javaForum/JavaForumApplicationTests.java
+++ src/test/java/com/leo/javaForum/javaForum/JavaForumApplicationTests.java
@@ -6,11 +6,14 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class JavaForumApplicationTests {
-
+	@Test
+	void goodTest() {
+		Assertions.assertTrue(true);
+	}
 
 	@Test
 	void testForActions() {
-		Assertions.assertEquals(1 , 9);
+		Assertions.assertEquals(1 , 1);
 	}
 
 }
